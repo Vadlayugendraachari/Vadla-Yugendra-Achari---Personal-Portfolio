5b17abd63ea1ef3228ae0f097986569b534de9c1
Vadlayugendraachari <yugendharachari285@gmail.com>
2025-08-19 16:13:30 +0530

    initail commit

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -2,7 +2,8 @@ import { Helmet } from 'react-helmet'
 
 export const SEO = () => (
     <Helmet>
-        <title>Vadla Yugendra Achari | Full Stack Developer</title>
+        <title>Vadla Yugendra Achari | Frontend Developer</title>
         <meta name="description" content="Full Stack Developer – Python & MERN. Projects: Community Forum, Househunt. Research at IIT Dharwad." />
     </Helmet>
 )
+
